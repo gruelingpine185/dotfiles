7b534dfd33c33495c06e630df929814682367ad6
gruelingpine185 <gruelingpine185@gmail.com>
2024-01-31 11:30:29 -0500

    added git and iterm through homebrew

---
+++ brew.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# update homebrew
+brew update
+
+# upate installed formulae
+brew upgrade
+
+# terminal programs
+brew install git
+brew install iterm2
+
+# remove outdated versions
+brew cleanup
