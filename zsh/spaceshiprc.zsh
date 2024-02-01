eae609c1281274daa06a44fd355e58a29b33023d
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-02-01 00:41:03 -0500

    altered spaceship git section

--- zsh/spaceshiprc.zsh
+++ zsh/spaceshiprc.zsh
@@ -5,3 +5,17 @@ SPACESHIP_PROMPT_ADD_NEWLINE=false
 
 # split prompt across 2 lines (default: true)
 SPACESHIP_PROMPT_SEPARATE_LINE=false
+
+# -- Git --
+
+# icon before git branch (default: "$SPACESHIP_GIT_SYMBOL")
+SPACESHIP_GIT_BRANCH_PREFIX=""
+
+# text between current git branch  (default: "on ")
+SPACESHIP_GIT_PREFIX=""
+
+# text before git status (default: " [")
+SPACESHIP_GIT_STATUS_PREFIX=" "
+
+# text after git status (default: "]")
+SPACESHIP_GIT_STATUS_SUFFIX=""
