7df81c774bb3cb3345a36adaed85522d678288e5
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-02-02 12:26:46 -0500

    added zathura

--- brew.sh
+++ brew.sh
@@ -9,12 +9,17 @@ brew update
 # upate installed formulae
 brew upgrade
 
+brew tap zegervdv/zathura
+
 # terminal programs
+brew install girara --HEAD
 brew install git
 brew install helix
 brew install iterm2
+brew install zathura-pdf-poppler
 brew install showkey
 brew install spaceship
+brew install zathura --HEAD
 
 # language servers
 brew install bash-language-server
