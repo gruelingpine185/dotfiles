31daefd5081da2d81baaf85052bfbca852ac882a
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-02-02 12:26:23 -0500

    Create setup.zsh

---
+++ zathura/setup.zsh
@@ -0,0 +1,14 @@
+expected_comment="# already setup [don't delete this line]"
+
+# read last line of this file and install poppler plugin for zathura if it
+# doesn't it doesn't match the expected comment
+if [[ $(tail -n 1 "$0") == "$expected_comment" ]]; then
+  exit 0
+fi
+
+mkdir -p $(brew --prefix zathura)/lib/zathura
+ln -s $(brew --prefix zathura-pdf-poppler)/libpdf-poppler.dylib $(brew --prefix zathura)/lib/zathura/libpdf-poppler.dylib
+printf "$expected_comment" >> "$0"
+
+exit 0
+# already setup [don't delete this line]
