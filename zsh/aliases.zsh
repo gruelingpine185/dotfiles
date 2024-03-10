b8e50ee438c26bdcbd5365c79a5f0d82d0c2ce3e
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-03-10 16:44:24 -0400

    added alias for github desktop

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
+alias gh="open -a Github\ Desktop"
+alias img="magick mogrify -monitor -format png *.heic"
 alias make="bear -- make"
 alias yt="yt-dlp --extract-audio --audio-format mp3"
-alias img="magick mogrify -monitor -format png *.heic"
