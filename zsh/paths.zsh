5ffe350cd22a1fb0516999c0c416426e05217014
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2024-11-22 19:24:21 -0500

    updated paths

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,1 +1,3 @@
-export PATH="${DOTFILE_PATH}/bin:${PATH}"
+export PATH="${DOTFILE_PATH}/bin:${HOME}/.local/bin:${PATH}"
+export BOOK_PATH="${HOME}/Documents/books"
+export VULKAN_SDK=$(echo ${HOME}/VulkanSDK/*/macOS)
